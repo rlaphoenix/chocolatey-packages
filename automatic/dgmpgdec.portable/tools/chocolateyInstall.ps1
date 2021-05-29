2b99943a81da239ba2d135eeab2664c026f830ed
PHOENiX <rlaphoenix@pm.me>
2021-05-29 06:02:11 +0100

    Create DGMPGDec project (manual for now)

---
+++ automatic/dgmpgdec.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop';
+$softwareName = 'DGMPGDec'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://rationalqm.us/dgmpgdec/dgmpgdec2005.zip'
+$checksum     = 'CB8F58F1CC88501512868BCDED6F4D4070EEEB7F5145EFA9045F0BB2BCD3904E'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = $softwareName
+
+  checksum      = $checksum
+  checksum64    = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$libDir = (Get-ChildItem $env:ALLUSERSPROFILE\chocolatey\lib\dgmpgdec* | select $_.last)
+$dgIndexPath = "$($libDir)\tools\DGIndex.exe"
+
+Install-ChocolateyFileAssociation ".d2v" $dgIndexPath
+
+$startDir = "$Env:ProgramData\Microsoft\Windows\Start Menu\Programs\DGIndex.lnk"
+Install-ChocolateyShortcut -ShortcutFilePath $startDir -TargetPath $dgIndexPath
---
+++ automatic/dgmpgdec.portable/update.ps1
@@ -0,0 +1,23 @@
+import-module au
+
+$releases = 'http://rationalqm.us/dgmpgdec/'
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regex   = 'dgmpgdec(\d{4}).zip$'
+    $url     = $download_page.links | ? href -match $regex | select -First 1 -expand href
+    $version = $url | Select-String -pattern $regex | % {$_.matches.groups[1].value}
+    $version = $version.ToCharArray() -join "."
+    return @{ Version = $version; URL32 = $url }
+}
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+ }
+ 
+update -ChecksumFor all -NoCheckUrl
