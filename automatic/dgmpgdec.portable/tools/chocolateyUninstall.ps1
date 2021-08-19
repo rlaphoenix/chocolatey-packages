86ab99cb5e2b8cb164168bd637956ce48ab20665
rlaphoenix <rlaphoenix@pm.me>
2021-08-19 16:00:32 +0100

    dgmpgdec: Re-write chocolateyUninstall script
    Uninstall-ChocolateyPath is apparently not a thing, so I had to do it this way instead.

--- automatic/dgmpgdec.portable/tools/chocolateyUninstall.ps1
+++ automatic/dgmpgdec.portable/tools/chocolateyUninstall.ps1
@@ -1,2 +1,22 @@
+$ErrorActionPreference = "Stop";
+
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-Uninstall-ChocolateyPath -PathToInstall $toolsDir -PathType "Machine" | Out-Null
+$packageName = 'dgmpgdec.portable'
+$zipName = 'dgmpgdec2005.zip'
+
+Uninstall-ChocolateyZipPackage $packageName $zipName
+
+#Removes duplicates of the target removal path, Cleans up double ";", Handles ending "\"
+#Credit: https://gitlab.com/DarwinJS/ChocoPackages/-/blob/master/ec2clitools/tools/chocolateyuninstall.ps1
+
+$PathToRemove = $toolsDir
+foreach ($path in [Environment]::GetEnvironmentVariable("PATH", "Machine").split(';')) {
+  If ($Path) {
+    If (($path -ine "$PathToRemove") -AND ($path -ine "$PathToRemove\")) {
+      [string[]]$Newpath += "$path"
+    }
+  }
+}
+$AssembledNewPath = ($newpath -join(';')).trimend(';')
+
+[Environment]::SetEnvironmentVariable("PATH", $AssembledNewPath, "Machine")
