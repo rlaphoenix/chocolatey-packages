dcccfa5ad778a5e5358181e4b52dbfe1deb4f660
rlaphoenix <rlaphoenix@pm.me>
2021-08-19 16:12:28 +0100

    bdinfo: Re-write chocolateyUninstall script

--- automatic/bdinfo.portable/tools/chocolateyUninstall.ps1
+++ automatic/bdinfo.portable/tools/chocolateyUninstall.ps1
@@ -1,2 +1,22 @@
-$installPath = "$env:ChocolateyInstall\lib\bdinfo.portable\tools\"
-Uninstall-ChocolateyPath $installPath 'Machine' | Out-Null
+$ErrorActionPreference = "Stop";
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$packageName = 'bdinfo.portable'
+$zipName = 'BDInfo_v0.7.5.6.zip'
+
+Uninstall-ChocolateyZipPackage $packageName $zipName
+
+#Removes duplicates of the target removal path, Cleans up double ";", Handles ending "\"
+#Credit: https://gitlab.com/DarwinJS/ChocoPackages/-/blob/master/ec2clitools/tools/chocolateyuninstall.ps1
+
+$PathToRemove = $toolsDir
+foreach ($path in [Environment]::GetEnvironmentVariable("PATH", "Machine").split(';')) {
+  If ($Path) {
+    If (($path -ine "$PathToRemove") -AND ($path -ine "$PathToRemove\")) {
+      [string[]]$Newpath += "$path"
+    }
+  }
+}
+$AssembledNewPath = ($newpath -join(';')).trimend(';')
+
+[Environment]::SetEnvironmentVariable("PATH", $AssembledNewPath, "Machine")
